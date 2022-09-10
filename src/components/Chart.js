3150cdeb398e6cda94529bfc1f188188aab13d1f
FernandoDanielZigarra <99737822+FernandoDanielZigarra@users.noreply.github.com>
2022-09-10 12:32:13 -0300

    enlazando componentes finalizado

---
+++ src/components/Chart.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ChartRow from './ChartRow';
+
+let tableRowsData = [
+    {
+        Title: 'Rambo',
+        Length: '120',
+        Rating: '7',
+        Categories: ['Acción'],
+        Awards: 2
+    },
+    {
+        Title: 'Alicia en el país de las maravillas',
+        Length: '142',
+        Rating: '4.8',
+        Categories: ['Drama','Acción','Comedia'],
+        Awards: 3
+    },
+    
+]
+
+
+function Chart (){
+    return (
+        /* <!-- DataTales Example --> */
+        <div className="card shadow mb-4">
+            <div className="card-body">
+                <div className="table-responsive">
+                    <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
+                        <thead>
+                            <tr>
+                                <th>Título</th>
+                                <th>Duración</th>
+                                <th>Rating</th>
+                                <th>Género</th>
+                                <th>Premios</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                            tableRowsData.map( ( row , i) => {
+                                return <ChartRow { ...row} key={i}/>
+                            })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Chart;
